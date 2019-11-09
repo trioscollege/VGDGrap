396ff5f07c13825ca02f3c8ca9b3c314e36c6db6
100Vikings <jeff.gauvin@trios.com>
2019-11-09 14:13:59 -0500

    Initial Chapter 3 commit.

---
+++ SDL_Template/GameManager.cpp
@@ -0,0 +1,77 @@
+#include "GameManager.h"
+
+GameManager * GameManager::sInstance = nullptr;
+
+GameManager * GameManager::Instance()
+{
+	if (sInstance == nullptr) {
+		sInstance = new GameManager();
+	}
+	return sInstance;
+}
+
+void GameManager::Release() {
+	delete sInstance;
+	sInstance = nullptr;
+}
+
+void GameManager::Run() {
+	while (!mQuit) {
+		mTimer->Update();
+
+		while (SDL_PollEvent(&mEvent)) {
+			switch (mEvent.type) {
+			case SDL_QUIT:
+				mQuit = true;
+				break;
+			}
+		}
+
+		if (mTimer->DeltaTime() >= 1.0f / FRAME_RATE) {
+			mGraphics->Render();
+			mTimer->Reset();
+		}
+	}
+}
+
+GameManager::GameManager() : mQuit(false) {
+	mGraphics = Graphics::Instance();
+
+	if (!Graphics::Initialized()) {
+		mQuit = true;
+	}
+
+	mTimer = Timer::Instance();
+
+	// sanity test
+	mParent = new GameEntity(100.0f, 400.0f);
+	mChild = new GameEntity(100.0f, 500.0f);
+
+	// print local position of mChild with no parent set
+	printf("Child local pos: (%f, %f)\n",
+		mChild->Position(GameEntity::Local).x,
+		mChild->Position(GameEntity::Local).y);
+
+	// set parent of mChild to mParent
+	mChild->Parent(mParent);
+	mParent->Rotation(90.0f);
+
+	// print local position of mChild with parent set
+	printf("Child local pos: (%f, %f)\n",
+		mChild->Position(GameEntity::Local).x,
+		mChild->Position(GameEntity::Local).y);
+	printf("Child world pos: (%f, %f)\n",
+		mChild->Position(GameEntity::World).x,
+		mChild->Position(GameEntity::World).y);
+}
+
+GameManager::~GameManager() {
+	Graphics::Release();
+	mGraphics = nullptr;
+
+	Timer::Release();
+	mTimer = nullptr;
+
+	// Quit SDL subsystems
+	SDL_Quit();
+}
---
+++ SDL_Template/GameManager.h
@@ -0,0 +1,35 @@
+#ifndef __GAMEMANAGER_H
+#define __GAMEMANAGER_H
+
+#include <SDL.h>
+#include "Graphics.h"
+#include "Timer.h"
+#include "GameEntity.h"
+
+class GameManager
+{
+private:
+	static GameManager * sInstance;
+	const int FRAME_RATE = 60;
+
+	bool mQuit;
+	Graphics * mGraphics;
+
+	Timer * mTimer;
+
+	// sanity test
+	GameEntity * mParent;
+	GameEntity * mChild;
+
+	SDL_Event mEvent;
+
+public:
+	static GameManager * Instance();
+	static void Release();
+
+	void Run();
+
+	GameManager();
+	~GameManager();
+};
+#endif
