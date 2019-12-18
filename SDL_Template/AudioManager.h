3d6faa849b89370ef60196866fca2ed56a65d609
100Vikings <jeff.gauvin@trios.com>
2019-12-18 00:44:06 -0500

    Update AudioManager
    Add overloads for PlayMusic and PlaySFX

--- SDL_Template/AudioManager.h
+++ SDL_Template/AudioManager.h
@@ -13,11 +13,13 @@ namespace SDLFramework {
 		static AudioManager * Instance();
 		static void Release();
 
-		void PlayMusic(std::string filename, int loops = -1, bool managed = true);
+		void PlayMusic(std::string filename, int loops = -1);
+		void PlayMusic(Mix_Music * music, int loops = -1);
 		void PauseMusic();
 		void ResumeMusic();
 
-		void PlaySFX(std::string filename, int loops = 0, int channel = -1, bool managed = true);
+		void PlaySFX(std::string filename, int loops = 0, int channel = -1);
+		void PlaySFX(Mix_Chunk * sfx, int loops = 0, int channel = -1);
 
 	private:
 		AudioManager();
