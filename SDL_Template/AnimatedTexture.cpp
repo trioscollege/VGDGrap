a914fe62aa2d797082311f219136c0150d406129
100Vikings <jeff.gauvin@trios.com>
2020-01-09 16:20:48 -0500

    Update AnimatedTexture

--- SDL_Template/AnimatedTexture.cpp
+++ SDL_Template/AnimatedTexture.cpp
@@ -1,6 +1,30 @@
 #include "AnimatedTexture.h"
 
 namespace SDLFramework {
+	void AnimatedTexture::RunAnimation() {
+		if (!mAnimationDone) {
+			mAnimationTimer += mTimer->DeltaTime();
+
+			if (mAnimationTimer >= mAnimationSpeed) {
+				if (mWrapMode == Loop) {
+					// reset timer, accounting for extra time
+					mAnimationTimer -= mAnimationSpeed;
+				}
+				else {
+					mAnimationDone = true;
+					// back up the timer to the last frame
+					mAnimationTimer = mAnimationSpeed - mTimePerFrame;
+				}
+			}
+
+			if (mAnimationDirection == Horizontal) {
+				mSourceRect.x = mStartX + (int)(mAnimationTimer / mTimePerFrame) * mWidth;
+			}
+			else {
+				mSourceRect.y = mStartY + (int)(mAnimationTimer / mTimePerFrame) * mHeight;
+			}
+		}
+	}
 
 	AnimatedTexture::AnimatedTexture(std::string filename, int x, int y, int w, int h, int frameCount, float animationSpeed, AnimDir animationDir, bool managed)
 		: Texture(filename, x, y, w, h, managed) {
@@ -26,28 +50,16 @@ namespace SDLFramework {
 		mWrapMode = wrapMode;
 	}
 
+	void AnimatedTexture::ResetAnimation() {
+		mAnimationTimer = 0.0f;
+		mAnimationDone = false;
+	}
+
+	bool AnimatedTexture::IsAnimating() {
+		return !mAnimationDone;
+	}
+
 	void AnimatedTexture::Update() {
-		if (!mAnimationDone) {
-			mAnimationTimer += mTimer->DeltaTime();
-
-			if (mAnimationTimer >= mAnimationSpeed) {
-				if (mWrapMode == Loop) {
-					// reset timer, accounting for extra time
-					mAnimationTimer -= mAnimationSpeed;
-				}
-				else {
-					mAnimationDone = true;
-					// back up the timer to the last frame
-					mAnimationTimer = mAnimationSpeed - mTimePerFrame;
-				}
-			}
-
-			if (mAnimationDirection == Horizontal) {
-				mSourceRect.x = mStartX + (int)(mAnimationTimer / mTimePerFrame) * mWidth;
-			}
-			else {
-				mSourceRect.y = mStartY + (int)(mAnimationTimer / mTimePerFrame) * mHeight;
-			}
-		}
+		RunAnimation();
 	}
 }
--- SDL_Template/AnimatedTexture.h
+++ SDL_Template/AnimatedTexture.h
@@ -12,7 +12,7 @@ namespace SDLFramework {
 		enum WrapMode { Once = 0, Loop = 1 };
 		enum AnimDir { Horizontal = 0, Vertical = 1 };
 
-	private:
+	protected:
 		Timer * mTimer;
 		int mStartX;
 		int mStartY;
@@ -28,11 +28,16 @@ namespace SDLFramework {
 
 		bool mAnimationDone;
 
+		virtual void RunAnimation();
+
 	public:
 		AnimatedTexture(std::string filename, int x, int y, int w, int h, int frameCount, float animationSpeed, AnimDir animationDir, bool managed = false);
 		~AnimatedTexture();
 
 		void SetWrapMode(WrapMode mode);
+
+		virtual void ResetAnimation();
+		bool IsAnimating();
 
 		void Update();
 	};
