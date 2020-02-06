939edae91016db5728fb159d279cc222f88f5926
100Vikings <jeff.gauvin@trios.com>
2020-02-06 14:27:57 -0500

    Update Scoreboard

--- SDL_Template/Scoreboard.h
+++ SDL_Template/Scoreboard.h
@@ -9,13 +9,16 @@ class Scoreboard : public GameEntity {
 private:
 	std::vector<Texture *> mScore;
 
+	SDL_Color mColor;
+
 public:
 	Scoreboard();
+	Scoreboard(SDL_Color color);
 	~Scoreboard();
 
 	void Score(int score);
 
-	void Render() override;
+	void Render();
 
 private:
 	void ClearBoard();
